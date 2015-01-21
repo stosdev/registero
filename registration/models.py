97e0886075aaa5becfbc7debe23d6e2dffd7c7b7
Paweł Jan Czochański <czochanski@gmail.com>
2015-01-21 10:52:53 +0100

    Beggining of registration models.

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,25 @@
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
+
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(_("Insititute name"), max_length=255)
+    name.help_text = _("The name of the school/university \
+                       providing this team.")
+    couch = models.ForeignKey(User, verbose_name=_("Couch"))
+
+    class Meta:
+        verbose_name = _("Team")
+        verbose_name_plural = _("Teams")
+
+
+class Participant(models.Model):
+    firstname = models.CharField(_("First name"), max_length=255)
+    lastname = models.CharField(_("Last name"), max_length=255)
+    team = models.ForeignKey(Team, verbose_name=_("Team"))
+
+    class Meta:
+        verbose_name = _("Participant")
+        verbose_name_plural = _("Participants")
