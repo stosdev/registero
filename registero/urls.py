65b947708f20c028f4084c25ffa14c956a186742
Paweł Jan Czochański <czochanski@gmail.com>
2015-02-08 21:04:16 +0100

    A few translation fixes and an url redirect.

--- registero/urls.py
+++ registero/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,4 +11,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^', include('news.urls')),
+    url(r'^accounts/profile/$',
+        RedirectView.as_view(url='/')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
