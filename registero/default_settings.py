8f663087cec1d311a77da11829f68ecc4177bf7e
Paweł Jan Czochański <czochanski@gmail.com>
2015-02-08 17:47:06 +0100

    Fixed activation email template.

--- registero/default_settings.py
+++ registero/default_settings.py
@@ -138,6 +138,8 @@ LANGUAGES = (
 )
 
 # Email settings
+# Email test command:
+# python -m smtpd -n -c DebuggingServer localhost:1025
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = '1025'
